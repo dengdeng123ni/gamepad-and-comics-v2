5ffcdc909f291cc6c6f09ade9b7026d4d31e0c46
等等123你 <5358997+dengdeng123ni@user.noreply.gitee.com>
2023-07-30 19:49:01 +0800

    更新

---
+++ src/app/pages/list/components/comics-list/comics-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+interface Item {
+  id: string | number,
+  cover: string,
+  title: string,
+  subTitle: string,
+  index?: number,
+  selected?: boolean,
+  tags?: Array<string>
+}
+@Component({
+  selector: 'comics-list',
+  templateUrl: './comics-list.component.html',
+  styleUrls: ['./comics-list.component.scss']
+})
+
+export class ComicsListComponent {
+
+  @Input() is_edit?: boolean = true;
+  @Input() list: Array<Item> = [];
+  @Input() size?: string = "middle"; // large middle small
+  @Input() last_read_id?: string | number = "";
+
+  @Output() on_item = new EventEmitter<{ $event: HTMLElement, data: any }>();
+
+  @Output() on_list = new EventEmitter<HTMLElement>();
+
+  on($event: MouseEvent) {
+    const node = $event.target as HTMLElement;
+    if (node.getAttribute("id") == 'list') {
+      this.on_list.emit(node);
+    } else {
+      const getTargetNode = (node: HTMLElement): HTMLElement => {
+        if (node.getAttribute("region") == "comics_list_item") {
+          return node
+        } else {
+          return getTargetNode(node.parentNode as HTMLElement)
+        }
+      }
+      const target_node = getTargetNode(node);
+      const index = parseInt(target_node.getAttribute("index") as string);
+      this.on_item.emit({ $event: target_node, data: { ...this.list[index], index } });
+    }
+  }
+}
--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ListRoutingModule } from './list-routing.module';
 import { IndexComponent } from './pages/index/index.component';
 import { MaterialModule } from 'src/app/library/material.module';
+import { ComicsListComponent } from './components/comics-list/comics-list.component';
+import { ToolBarComponent } from './complex-components/tool-bar/tool-bar.component';
 
 
 @NgModule({
   declarations: [
-    IndexComponent
+    IndexComponent,
+    ComicsListComponent,
+    ToolBarComponent
   ],
   imports: [
     CommonModule,
